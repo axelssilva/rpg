565894f93361d9d3844ef13af5bc289847cba6b3
Axel Silva <contato.axelsls@gmail.com>
2023-02-02 16:05:19 -0300

    02/02/2023_2

--- dano_rpg.py
+++ dano_rpg.py
@@ -1,33 +1,49 @@
+#  da mesma forma que antes vamos utilizar variaveis para definir valores, sendo elas as variaveis:
+#  dado - a quantidade de faces que o dado de dano possúi
+#  quantidaded - representa quantos dados devem ser rolados para o seu dano
+#  moda - é o modificador usado no ataque
+#  vantagem - é o modificador utilizado quando for rolar o dano
+#  margem - o menor valor necessário para que seja dado um ataque critico
+#  multiplicador - por quantas vezes o tipo de dado sera multiplicado no caso de um critico
+#  n - a quantidade de vezes em que sera testado
+
+#  módulos utilizados:
 from random import randint
+from math import floor
 
-#dados representa qual o formato de dado utilizado (d4, d6, d8,..),
-#qtd representa a quantidade de dados utilizada na rolagem de dano,
-#modificador representa a modificação de dano de acordo com o atributo utilizado para ataque (força, destreaza,...),
-#teste é o modificador de teste utilizado no dano, por exemplo, luta, pontaria, etc
-#margem é a margem de critico
-#multiplicador é a quantidade de vezes em que é multiplicado o dano de acordo com o critico
-#por fim, n é a quantidade de vezes que o dano será testado
+#  variáveis:
+dado = int(input('Qual o tipo de dado? '))
+quantidaded = int(input('Quantos dados são? '))
+moda = int(input('Qual o seu modificador de ataque? '))
+vantagem = int(input('Qual o seu bonus de dano? '))
+margem = int(input('Qual sua margem para crítico? '))
+multiplicador = int(input('Qual o seu multiplicador de crítico? '))
+n = int(input('Quantas vezes quer testar? '))
+rold = randint(1, dado)
 
-dados = int(input(f'Qual o dado do seu dano? d '))
-quantidade = int(input('Quantos dados de dano tem o seu ataque? '))
-modificador = int(input('Qual o modificador de dano? '))
-bonus = int(input('Bonus do teste de ataque: '))
-margem = int(input('Qual o menor valor para crítico? '))
-multiplicador = int(input('Qual o seu múltiplicador de crítico? '))
-n = int(input('Quantos testes quer fazer? '))
+#  contadores:
+totald = 0
 ataque = 0
-dano = 0
-#teste de ataque
-for a in range(1,n+1):
-    ataque = randint(20,21)
-#dano
-    if ataque >= margem:
-        for b in range(1, quantidade + multiplicador + 1):
-            dano = randint(1,dados+1)+modificador
+
+#  script:
+for t in range(1, n+1):
+    rol = randint(1,20)
+    if rol >= margem:
+        for d in range(1,(quantidaded*multiplicador)+1):
+            dano = randint(1, dado)
+            totald += dano
     else:
-        for b in range(1, quantidade+1):
-            dano = randint(1, dados+1)+modificador
-        ataque += a
-        dano += b
-print(f'Sua média de dano é {dano}\nE sua média de ataque é {(ataque)+bonus}')
-#usando laço for com um contador zerado no inicio para somar os valores de ataque e de dano para no fim fazer a média deles, testes do dia 02/02/2023, mas os valores ainda não fecham, novos testes serão feitos
+        for d in range(1, quantidaded):
+            dano = randint(1, dado)
+            totald += dano
+    ataque += rol
+
+#  saidas:
+print(f'''===========================
+A sua média de ataque é {floor((ataque/n)+moda)}
+A sua média de dano é {floor((totald/n)+vantagem)}
+===========================''')
+
+#  o script está funcional usando laço for, o próximo passo é atualizar o mesmo para o fromato while,
+#  a ideia é poder repetir o script quantas vezes desejar 
+#  02/02/2023
