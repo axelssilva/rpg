79ca6e08431586e67e26c042d45a4ed280840c4a
Axel Silva <contato.axelsls@gmail.com>
2023-02-02 16:32:14 -0300

    Update dano_rpg.py

--- dano_rpg.py
+++ dano_rpg.py
@@ -8,42 +8,45 @@
 #  n - a quantidade de vezes em que sera testado
 
 #  módulos utilizados:
+
+
 from random import randint
 from math import floor
 
+resposta = 'S'
+while resposta in 'Ss':
 #  variáveis:
-dado = int(input('Qual o tipo de dado? '))
-quantidaded = int(input('Quantos dados são? '))
-moda = int(input('Qual o seu modificador de ataque? '))
-vantagem = int(input('Qual o seu bonus de dano? '))
-margem = int(input('Qual sua margem para crítico? '))
-multiplicador = int(input('Qual o seu multiplicador de crítico? '))
-n = int(input('Quantas vezes quer testar? '))
-rold = randint(1, dado)
+    dado = int(input('Qual o tipo de dado? '))
+    quantidaded = int(input('Quantos dados são? '))
+    moda = int(input('Qual o seu modificador de ataque? '))
+    vantagem = int(input('Qual o seu bonus de dano? '))
+    margem = int(input('Qual sua margem para crítico? '))
+    multiplicador = int(input('Qual o seu multiplicador de crítico? '))
+    n = int(input('Quantas vezes quer testar? '))
+    rold = randint(1, dado)
 
 #  contadores:
-totald = 0
-ataque = 0
+    totald = 0
+    ataque = 0
 
 #  script:
-for t in range(1, n+1):
-    rol = randint(1,20)
-    if rol >= margem:
-        for d in range(1,(quantidaded*multiplicador)+1):
-            dano = randint(1, dado)
-            totald += dano
-    else:
-        for d in range(1, quantidaded):
-            dano = randint(1, dado)
-            totald += dano
-    ataque += rol
-
-#  saidas:
-print(f'''===========================
+    for t in range(1, n+1):
+        rol = randint(1,20)
+        if rol >= margem:
+            for d in range(1,(quantidaded*multiplicador)+1):
+                dano = randint(1, dado)
+                totald += dano
+        else:
+            for d in range(1, quantidaded):
+                dano = randint(1, dado)
+                totald += dano
+        ataque += rol
+    resposta = input(f'''===========================
 A sua média de ataque é {floor((ataque/n)+moda)}
 A sua média de dano é {floor((totald/n)+vantagem)}
-===========================''')
+===========================
+repetir? [s/n] ''').upper()
+print('===========================')
 
-#  o script está funcional usando laço for, o próximo passo é atualizar o mesmo para o fromato while,
-#  a ideia é poder repetir o script quantas vezes desejar 
-#  02/02/2023
+# o scrip está funcionando utilizando laço while porém de maneira pouco otimizada aninhando o script com laços for
+# a um laço while e consequentemente ocupando memória desnecessária. 02/02/2023
