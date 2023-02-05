d984e1a9ab4bce9d54513187b5d556d2ea1caf92
Axel Silva <contato.axelsls@gmail.com>
2023-02-05 18:27:26 -0300

    Update dano_partida.py

--- dano_partida.py
+++ dano_partida.py
@@ -1,29 +1,58 @@
-from random import randint
-from math import floor
+#  da mesma forma que antes vamos utilizar variaveis para definir valores, sendo elas as variaveis:
+#  dado - a quantidade de faces que o dado de dano possúi
+#  quantidaded - representa quantos dados devem ser rolados para o seu dano
+#  moda - é o modificador usado no ataque
+#  vantagem - é o modificador utilizado quando for rolar o dano
+#  margem - o menor valor necessário para que seja dado um ataque critico
+#  multiplicador - por quantas vezes o tipo de dado sera multiplicado no caso de um critico
 
-dado = int(input('Qual o tipo de dado? '))
-quantidaded = int(input('Quantos dados são? '))
-moda = int(input('Qual o seu modificador de ataque? '))
-vantagem = int(input('Qual o seu bonus de dano? '))
-margem = int(input('Qual sua margem para crítico? '))
-multiplicador = int(input('Qual o seu multiplicador de crítico? '))
-totald = 0
-while True:
-    rol = randint(1,20)
+#  módulos utilizados:
+from random import randint
+
+# defs utilizadas:
+def parâmetros():
+    global dado, quantidaded, moda, vantagem, margem, multiplicador
+    dado = int(input('Qual o tipo de dado? '))
+    quantidaded = int(input('Quantos dados são? '))
+    moda = int(input('Qual o seu modificador de ataque? '))
+    vantagem = int(input('Qual o seu bonus de dano? '))
+    margem = int(input('Qual sua margem para crítico? '))
+    multiplicador = int(input('Qual o seu multiplicador de crítico? '))
+
+
+def roll():
+    global totald, rol
+    totald = 0
+    rol = randint(1, 20)
     if rol >= margem:
-        for d in range(1,(quantidaded*multiplicador)+1):
+        for d in range(1, (quantidaded * multiplicador) + 1):
             dano = randint(1, dado)
             totald += dano
     else:
-        for d in range(1, quantidaded+1):
+        for d in range(1, quantidaded + 1):
             dano = randint(1, dado)
             totald += dano
-    resposta = input(f'''===========================
-Ataque: {floor((rol)+moda)}
-Dano: {floor((totald)+vantagem)}
-===========================
-Atacar? [s/n] ''').upper().strip()[0]
-    print('===========================')
-    if resposta in 'Nn':
+
+#script principal:
+dado = quantidaded = moda = vantagem = margem = multiplicador = resp = totald = 0
+parâmetros()
+while True:
+    print('=-=' * 30)
+    resp = int(input('''Selecione uma opção:
+        [1] Rolar ataque
+        [2] Redefinir valores
+        [0] Sair do programa
+        Sua decisão: '''))
+    print('=-='*30)
+    if resp == 1:
+        roll()
+        print(f'Seu roll puro foi {rol}\nSeu ataque foi {rol+moda}\nSeu dano puro foi {totald}\nSeu dano foi {totald+vantagem}')
+    elif resp == 2:
+        parâmetros()
+    elif resp == 0:
+        print('Até mais!')
         break
-print('Obrigado, até mais!')
+    else:
+        print('comando incorreto, tente novamente!')
+
+#sacript funcional utilizandos defs 05/02/2023
